ff5bbdaf250b406ae4cc58e9dcf6b25e6924c132
Frederica Bernkastel <frederica.bernkastel0@gmail.com>
2024-03-04 18:32:44 +0600

    settings persistence

--- js/main.js
+++ js/main.js
@@ -1,20 +1,57 @@
+var settings = {
+  get: function (key) { return JSON.parse(window.localStorage.getItem(key)); },
+  set: function (key, value) { window.localStorage.setItem(key, JSON.stringify(value)); },
+  toggle: function (key) {
+    var v = !JSON.parse(window.localStorage.getItem(key));
+    window.localStorage.setItem(key, JSON.stringify(v));
+    return v;
+  }
+}
+
 $(function () {
-  $('[type="checkbox"]#bgm').on('change', function () {
-    $('html').toggleClass('bgm')
-  });
-  $('[type="checkbox"]#meta1').on('change', function () {
-    if (window.localStorage.getItem('alert_meta1') !== '1') {
-      if ($(this).is(':checked')) {
-        alert('Not recommended on the first playthrough.')
+  if (settings.get('bgm')) { $('html').addClass('bgm') }
+  $('[type="checkbox"]#bgm')
+    .prop('checked', settings.get('bgm'))
+    .on('change', function () {
+      $('html').toggleClass('bgm');
+      settings.set('bgm', $(this).is(':checked'))
+    });
+
+  if (settings.get('meta1')) { $('html').addClass('meta1') }
+  $('[type="checkbox"]#meta1')
+    .prop('checked', settings.get('meta1'))
+    .on('change', function () {
+      if (settings.get('alert_meta1') !== '1') {
+        if ($(this).is(':checked')) {
+          alert('Not recommended on the first playthrough.')
+        }
+        settings.set('alert_meta1', '1')
       }
-      window.localStorage.setItem('alert_meta1', '1')
-    }
-    $('html').toggleClass('meta1')
-  });
-  $('[type="checkbox"]#meta2').on('change', function () {
-    $('html').toggleClass('meta2')
-  });
-  $('[type="checkbox"]#meta3').on('change', function () {
-    $('html').toggleClass('meta3')
-  });
+      settings.toggle('meta1');
+      $('html').toggleClass('meta1')
+    });
+
+  if (settings.get('meta2')) { $('html').addClass('meta2') }
+  $('[type="checkbox"]#meta2')
+    .prop('checked', settings.get('meta2'))
+    .on('change', function () {
+      settings.toggle('meta2');
+      $('html').toggleClass('meta2')
+    });
+
+  if (settings.get('meta3')) { $('html').addClass('meta3') }
+  $('[type="checkbox"]#meta3')
+    .prop('checked', settings.get('meta3'))
+    .on('change', function () {
+      settings.toggle('meta3');
+      $('html').toggleClass('meta3')
+    });
+
+  (function () {
+    var i = 1;
+    $('page').each(function () {
+      $(this).attr('data-id', i);
+      i = i + 1;
+    });
+  })();
 });
